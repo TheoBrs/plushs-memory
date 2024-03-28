966b5af61cfa4ba59026064e3063c2d3c6eb98f8
Kévin <75180055+KevinTerrisson@users.noreply.github.com>
2024-03-28 17:26:16 +0100

    beggin end menu

---
+++ Assets/Scripts/Actions/EndMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _mainActionsObject;
+    [SerializeField] private GameObject _optionActionsObject;
+
+    private void Awake()
+    {
+        if (!_mainActionsObject.activeSelf)
+        {
+            _mainActionsObject.SetActive(true);
+        }
+        if (_optionActionsObject.activeSelf)
+        {
+            _optionActionsObject.SetActive(false);
+        }
+    }
+
+    private void Start()
+    {
+        AudioManager.Instance.PlayMusic("MusicTest");
+    }
+
+    public void RestartGame()
+    {
+        ScenesManager.Instance.ExitGame();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
