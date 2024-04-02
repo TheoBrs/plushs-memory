518e603d05de16108275d5172f40fe425a476099
Kévin <75180055+KevinTerrisson@users.noreply.github.com>
2024-04-02 11:47:35 +0200

    Clear all death code

--- Assets/Scripts/UI/WinOrLose.cs
+++ Assets/Scripts/UI/WinOrLose.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,7 +7,7 @@ public class WinOrLose : MonoBehaviour
     public bool isWin = false;
     [SerializeField] Text Win;
     [SerializeField] Text Lose;
-    // Start is called before the first frame update
+
     void Start()
     {
         if (isWin == true) 
