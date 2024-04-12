55f82675d69cc4fdb1583baff904ea1306b95be9
OshidaBCF <swann.sanchez9@outlook.fr>
2024-04-12 16:36:33 +0200

    Disable movement and turn button during enemy turn

--- Assets/Scripts/UI/FloatingText.cs
+++ Assets/Scripts/UI/FloatingText.cs
@@ -8,7 +8,6 @@ public class FloatingText : MonoBehaviour
     public void Init(string text, Color color)
     {
         Destroy(gameObject, animation.length);
-        Debug.Log(animation.length);
         GetComponent<TextMesh>().text = text;
         GetComponent<TextMesh>().color = color;
     }
