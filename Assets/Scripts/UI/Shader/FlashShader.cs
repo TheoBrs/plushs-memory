ceab8d09acb336ec7d3d54e635ff280e6623516c
Kévin <75180055+KevinTerrisson@users.noreply.github.com>
2024-04-15 14:05:40 +0200

    fix uv outline

---
+++ Assets/Scripts/UI/Shader/FlashShader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashShader : MonoBehaviour
+{
+    [SerializeField] private Color _flashColor = Color.white;
+    [SerializeField] private float _flashTime = 0.25f;
+
+    private Material _material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
