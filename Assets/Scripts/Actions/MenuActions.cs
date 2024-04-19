aeea0f7c4c6c0b997d450115f5721b5e40419397
OshidaBCF <swann.sanchez9@outlook.fr>
2024-04-19 17:55:41 +0200

    sefq

--- Assets/Scripts/Actions/MenuActions.cs
+++ Assets/Scripts/Actions/MenuActions.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 
 public class MenuActions : MonoBehaviour
 {
-    [SerializeField] Animator animator;
     private void Start()
     {
-        animator.SetTrigger("StartFadeOut");
         //AudioManager.Instance.PlayMusic("MusicTest");
     }
 
